14fca7aa521ca2e660fc52a4e4c9b800632f1711
Tim Park <timfpark@gmail.com>
2017-01-31 12:48:11 -0800

    iterate through parallelism

--- server.js
+++ server.js
@@ -4,46 +4,63 @@ const async = require('async'),
 
 const USERS_ENDPOINT = "http://user.rhom.io/users";
 
-const PARALLEL_REQUESTS = 20;
-let requestIds = [];
-for (let idx = 0; idx < PARALLEL_REQUESTS; idx++) {
-    requestIds.push(idx);
-}
-
 let accessToken = jwt.sign({
     iss: '10152875766888406'
 }, process.env.ACCESS_TOKEN_SIGNING_KEY, {
     expiresIn: process.env.ACCESS_TOKEN_LIFETIME
 });
 
-let totalRequests = 0;
-let totalLatency = 0;
+const SETTLE_ITERATIONS = 30;
+const MAX_PARALLEL_REQUESTS = 50;
 
-async.each(requestIds, (requestId, requestCallback) => {
-    async.whilst(
-        () => { return true; },
-        callback => {
-            let startTimestamp = Date.now();
-            request.get(USERS_ENDPOINT, {
-                headers: {
-                    Authorization: "Bearer " + accessToken,
-                },
-                json: true,
-            }, (err, resp) => {
-                if (!err) {
-                    totalRequests += 1;
-                    totalLatency += (Date.now() - startTimestamp);
+function measureLatency(parallelism, callback) {
+    let totalRequests = 0;
+    let totalLatency = 0;
 
-                    if (totalRequests % 100 === 0) {
-                        let averageLatency = totalLatency / totalRequests;
-                        console.log(averageLatency);
+    let threads = [];
+    for (let idx = 0; idx < parallelism; idx++) {
+        threads.push(idx);
+    }
+
+    async.each(threads, (thread, threadCallback) => {
+        let iterationCount = 0;
+        async.whilst(
+            () => { return iterationCount < SETTLE_ITERATIONS; },
+            callback => {
+                iterationCount += 1;
+                let startTimestamp = Date.now();
+                request.get(USERS_ENDPOINT, {
+                    headers: {
+                        Authorization: "Bearer " + accessToken,
+                    },
+                    json: true,
+                }, (err, resp) => {
+                    if (!err) {
+                        totalRequests += 1;
+                        totalLatency += (Date.now() - startTimestamp);
                     }
-                }
 
-                return callback();
-            });
-        },
-        (err, n) => {
-        }
-    );
-});
+                    return callback();
+                });
+            },
+            threadCallback
+        );
+    }, err => {
+        let averageLatency = totalLatency / totalRequests;
+        console.log(`${parallelism}: ${averageLatency}`);
+        return callback();
+    });
+}
+
+let parallelRequests = 0;
+
+async.whilst(
+    () => { return parallelRequests < MAX_PARALLEL_REQUESTS; },
+    callback => {
+        parallelRequests += 1;
+        measureLatency(parallelRequests, callback);
+    },
+    process.exit
+);
+
+
